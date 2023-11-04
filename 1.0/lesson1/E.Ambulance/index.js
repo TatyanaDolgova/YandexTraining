7ce0588d118a8eb97e09a429279d582a6ca08a4a
Tatyana Dolgova <kylikovatatyana1993@mail.ru>
2023-11-04 23:27:28 +0500

    add some changer for ambulance task

--- 1.0/lesson1/E.Ambulance/index.js
+++ 1.0/lesson1/E.Ambulance/index.js
@@ -8,13 +8,14 @@ const numApart = Math.ceil(k2 / (n2 * p2));
 
 const kMax = Math.floor((+k2 - 1) / (+m * (+p2 - 1) + +n2 - 1));
 const kMin = Math.ceil(+k2 / (+m * (+p2 - 1) + +n2));
-const numApartInP = numApart * +m;
+const numApartInP = kMin * +m;
 
 const nAll = (m * p2) + +n2;
 
 if (kMin === kMax) {
   const p1 = Math.ceil(k1 / numApartInP);
-  let n1 = Math.ceil((k1 % numApartInP) / numApart);
+  let n1 = Math.ceil((k1 % numApartInP) / kMax);
+  console.log((k1 % numApartInP) / kMax)
 
   if (n1 === 0) {
     n1 = m;
@@ -31,20 +32,19 @@ if (kMin === kMax) {
   } else {
     result += p1Max;
   }
-
-  const n1 = Math.ceil((k1 % numApartInP) / numApart);
-  result += ' ' + n1;
+  const n1Max = Math.ceil((k1 % (kMax * m)) / kMax);
+  const n1Min = Math.ceil((k1 % (kMin * m)) / kMin);
+  if (n1Max !== n1Min) {
+    result += ' 0';
+  } else {
+    result += ' ' + n1Max;
+  }
 }
 
-if (+n2 > +m) {
-  result = '-1 -1'
-}
 
-if (+k2 < +n2) {
-  result = '-1 -1'
-}
 
 if (+n2 === 1) {
+
   if (+m >= +k1) {
     result = '1 0';
   } else if (+m === 1) {
@@ -67,13 +67,21 @@ if (+n2 === 1) {
       } else {
         result = '0';
         if (n1Max !== n1Min) {
-          result += '0 ';
+          result += ' 0';
         } else {
           result += n1Max;
         }
       }
     }
   }
+}
+
+// if (+p2 === 1 && (k2 * m) > +k1) {
+//   result = '1 0';
+// }
+
+if (+p2 === 1 && +n2 === 1 && +k1 < +k2) {
+  result = '1 1';
 }
 
 if (+k2 === +p2 && +m === 1 && +p2 > 1) {
@@ -85,5 +93,17 @@ if (p2 > 1) {
     result = '-1 -1';
   }
 }
+
+if (+n2 > +m) {
+  result = '-1 -1'
+}
+
+if (+k2 < +n2) {
+  result = '-1 -1'
+}
+
+// if (+k2 === +k1) {
+//   result = `${p2} ${n2}`;
+// }
 
 fs.writeFileSync('output.txt', result.toString());
